6c8836705c0c93af0519b7a133272ac6cf5f0a15
Sharon <sharon.peishan.kennedy@gmail.com>
2016-05-15 01:51:40 -0400

    added formspree

--- build/js/hbs.js
+++ build/js/hbs.js
@@ -1,6 +1,26 @@
 this["Marley"] = this["Marley"] || {};
 this["Marley"]["Templates"] = this["Marley"]["Templates"] || {};
 
+this["Marley"]["Templates"]["bigFooter"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+
+  return "            <li><a data-scroll href=\""
+    + alias4(((helper = (helper = helpers.link || (depth0 != null ? depth0.link : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"link","hash":{},"data":data}) : helper)))
+    + "\">"
+    + alias4(((helper = (helper = helpers.text || (depth0 != null ? depth0.text : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"text","hash":{},"data":data}) : helper)))
+    + "</a></li>\n";
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return "  <div class=\"footer-copy animated wow bounceInDown\" data-wow-duration=\"1000ms\" data-wow-delay=\"800ms\">\n    <div class=\"container\">\n      <div class=\"footer-copy-grids\">\n        <div class=\"col-md-3 footer-copy-grid\">\n          <h3><span>How</span> they're made</h3>\n          <img src=\"../images/6.jpg\" alt=\" \" class=\"img-responsive\" />\n          <p>Read more about the ingredients and processes we use to make Marley's Snacks.</p>\n        </div>\n        <div class=\"col-md-6 footer-copy-grid\">\n          <h3>Send a <span>message</span></h3>\n          <form action=\"https://formspree.io/sharonkennedy100@gmail.com\">\n            <input type=\"text\" name=\"name\" value=\"Name\" onfocus=\"this.value = '';\" onblur=\"if (this.value == '') {this.value = 'Name';}\" required=\"\">\n            <input type=\"email\" name=\"_replyto\" value=\"Email\" onfocus=\"this.value = '';\" onblur=\"if (this.value == '') {this.value = 'Email';}\" required=\"\">\n            <textarea type=\"text\" name=\"message\" onfocus=\"this.value = '';\" onblur=\"if (this.value == '') {this.value = 'Message...';}\" required=\"\">Message...</textarea>\n            <input type=\"text\" name=\"_gotcha\" style=\"display:none\" />\n            <input type=\"submit\" value=\"Submit\" >\n          </form>\n        </div>\n\n        <div class=\"col-md-3 footer-copy-grid\">\n          <h3>Navigation</h3>\n          <ul>\n"
+    + ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.navigation : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "          </ul>\n        </div>\n        <div class=\"clearfix\"> </div>\n      </div>\n    </div>\n  </div>";
+},"useData":true});
+
+this["Marley"]["Templates"]["contactSection"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "<div class=\"footer-top animated wow zoomInDown\" data-wow-duration=\"1000ms\" data-wow-delay=\"800ms\">\n  <div class=\"container\">\n    <h3>For more information feel free to <span>call or email</span></h3>\n    <p>Ask for <span>Robin :)</span></p>\n    <div class=\"more\">\n      <a href=\"contact.html\">Contact Us</a>\n    </div>\n  </div>\n</div>";
+},"useData":true});
+
 this["Marley"]["Templates"]["header"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     return "  <div class=\"banner-figures\">\n    <div class=\"banner\">\n      <div class=\"container banner-drop\">\n        <div class=\"header\"> \n            <!-- append left-header template -->\n            <!-- append right-header template -->\n            <div class=\"clearfix\"> </div>\n        </div>\n        <div class=\"logo animated wow bounceInDown\" data-wow-duration=\"1000ms\" data-wow-delay=\"500ms\">\n          <h1><a href=\"#\"><span>High quality raw dog treats</span>Marley's Snacks</a></h1>\n        </div>\n        <div class=\"social-icons animated wow bounceInDown\" data-wow-duration=\"1000ms\" data-wow-delay=\"800ms\">\n          <ul>\n            <li><a href=\"https://www.facebook.com/groups/717613388317899/\" class=\"facebook\"></a></li>\n          </ul>\n        </div>\n      </div>\n    </div>\n  </div>";
 },"useData":true});
@@ -31,8 +51,30 @@ this["Marley"]["Templates"]["headerRight"] = Handlebars.template({"1":function(c
     var stack1;
 
   return "<div class=\"header-right\">\n  <nav>\n    <ul>\n"
-    + ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.menuItems : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.navigation : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
     + "    </ul>\n  </nav>\n  <div class=\"menu-icon animated wow zoomIn\" data-wow-duration=\"1000ms\" data-wow-delay=\"800ms\"><span></span></div>\n</div>\n<script>\n  (function($){\n    $(\".menu-icon\").on(\"click\", function(){\n      $(this).toggleClass(\"open\");\n      $(\".container\").toggleClass(\"nav-open\");\n      $(\"nav ul li\").toggleClass(\"animate\");\n    });\n    \n  })(jQuery);\n</script>";
+},"useData":true});
+
+this["Marley"]["Templates"]["littleFooter"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "<div class=\"footer animated wow bounce\" data-wow-duration=\"1000ms\" data-wow-delay=\"800ms\">\n  <div class=\"container\">\n    <p>© 2016 Marley's Snacks. All rights reserved | Design by <a href=\"http://w3layouts.com\">W3layouts</a></p>\n  </div>\n</div>";
+},"useData":true});
+
+this["Marley"]["Templates"]["productDetails"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+
+  return "  <div class=\"col-md-4 team-grid animated wow bounceInLeft\" data-wow-duration=\"1000ms\" data-wow-delay=\"500ms\">\n    <div class=\"team-grid1 hover14 column\">\n      <div>\n        <figure><img src=\"../images/"
+    + alias4(((helper = (helper = helpers.image || (depth0 != null ? depth0.image : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"image","hash":{},"data":data}) : helper)))
+    + "\" alt=\" \" class=\"img-responsive\" /></figure>\n      </div>\n    </div>\n    <div class=\"team-grid1-sub\">\n      <h4>"
+    + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
+    + "</h4>\n      <p>"
+    + alias4(((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"description","hash":{},"data":data}) : helper)))
+    + "</p>\n      <div class=\"social-icons1 prices\">\n        <ul>\n          <li>"
+    + alias4(((helper = (helper = helpers.price || (depth0 != null ? depth0.price : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"price","hash":{},"data":data}) : helper)))
+    + "</li>\n        </ul>\n      </div>\n    </div>\n  </div>\n";
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.products : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
 },"useData":true});
 
 this["Marley"]["Templates"]["products"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
--- build/js/templates.js
+++ build/js/templates.js
@@ -5,7 +5,7 @@ $(function() {
                   email: "marleyssnacks@gmail.com"
                 }
 
-  var headerRightContext = { menuItems: [
+  var navigationContext = { navigation: [
       { link: 'index.html', icon: 'glyphicon-home', text: 'Home' },
       { link: 'index.html#products', icon: 'glyphicon-list', text: 'Products' },
       { link: 'index.html#locations-map', icon: 'glyphicon-map-marker', text: 'Where to buy' }, 
@@ -16,6 +16,7 @@ $(function() {
   var productsContext = { products: [
       { 
         itemsub: "item-sub",
+        image: '1.jpg',
         title: 'Duck feet',
         description: 'The hollow bones are great for cleaning your dog\'s teeth.',
         price: '$35 for 1 kilo'
@@ -23,11 +24,13 @@ $(function() {
       { 
         itemsub: "item-sub1",
         title: 'Chicken feet',
+        image: 'chicken_feet.jpg',
         description: 'The hollow bones are great for cleaning your dog\'s teeth.',
         price: '$30 for 1 kilo'
       },
       { 
         itemsub: "item-sub2",
+        image: 'beef_liver.jpg',
         title: 'Beef liver',
         description: 'Nutrient rich and breaks in to smaller pieces for easy carrying.',
         price: '$40 for 1 kilo'
@@ -35,12 +38,14 @@ $(function() {
       { 
         itemsub: "item-sub3",
         title: 'Salmon tails',
+        image: 'beef_liver.jpg',
         description: 'Delicious, crunchy and full of healthy fats.',
         price: '$40 for 1 kilo'
       },
       { 
         itemsub: "item-sub4",
         title: 'Sweet potato slices',
+        image: 'sweet_potato.jpg',
         description: 'Specially made for sweet doggies.',
         price: '$27 for 1 kilo'
       },
@@ -48,6 +53,10 @@ $(function() {
   }
 
   $('div.header').append(Marley.Templates.headerLeft(headerLeftContext));
-  $('div.header').append(Marley.Templates.headerRight(headerRightContext));
+  $('div.header').append(Marley.Templates.headerRight(navigationContext));
   $('div.product-list').append(Marley.Templates.products(productsContext));
+  $('div.product-details').append(Marley.Templates.productDetails(productsContext))
+  $('div.contact-section').append(Marley.Templates.contactSection({}))
+  $('div.big-footer').append(Marley.Templates.bigFooter(navigationContext))
+  $('div.little-footer').append(Marley.Templates.littleFooter({}))
 })
